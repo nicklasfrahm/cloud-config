326fed66a95e81258faca83dcf38e7ccbbb9be9c
Nicklas Frahm <nicklas.frahm@gmail.com>
2024-11-29 23:28:26 +0100

    fix(scripts): create flux templates

--- scripts/region-create.sh
+++ scripts/region-create.sh
@@ -1,47 +1,105 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-# Check if necessary tools are installed.
-tools=(talosctl sops)
-for tool in "${tools[@]}"; do
-  if ! command -v "$tool" &>/dev/null; then
-    echo "error: failed to find tool: $tool"
+region=""
+region_dir=""
+
+# tmp_dir="$(mktemp -d)"
+tmp_dir="."
+
+cleanup() {
+  # Ignore errors during cleanup.
+  rm -rf "$tmp_dir" || true
+}
+
+# trap cleanup EXIT SIGINT
+
+preflight_checks() {
+  # Check if necessary tools are installed.
+  tools=(talosctl sops gh flux ssh-keygen)
+  for tool in "${tools[@]}"; do
+    if ! command -v "$tool" &>/dev/null; then
+      echo "error: failed to find tool: $tool"
+      exit 1
+    fi
+  done
+
+  if [ $# -lt 1 ]; then
+    echo "Usage: $0 <region>"
     exit 1
   fi
-done
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <region>"
-  exit 1
-fi
+  region="$1"
+  region_dir="./configs/regions/$region"
 
-region="$1"
-mkdir -p "configs/regions/$region"
+  mkdir -p "$region_dir"
+}
 
-# Generate deploy key for the region.
-deploy_key_file="configs/regions/$region/flux.id_ed25519.key"
-if [[ ! -f "$deploy_key_file" ]]; then
-  echo "info: generating deploy key for region: $region"
+configure_deploy_key() {
+  deploy_key_file="$region_dir/flux.id_ed25519.key"
+  if [[ ! -f "$deploy_key_file" ]]; then
+    echo "info: generating deploy key for region: $region"
 
-  ssh-keygen -t ed25519 -f "$deploy_key_file" -C "$region" -N ""
-fi
-if [[ ! -f "${deploy_key_file/.key/.sops.key}" ]]; then
-  sops --encrypt "$deploy_key_file" >"${deploy_key_file/.key/.sops.key}"
-fi
+    ssh-keygen -t ed25519 -f "$deploy_key_file" -C "$region" -N ""
+  fi
+  if [[ ! -f "${deploy_key_file/.key/.sops.key}" ]]; then
+    sops --encrypt "$deploy_key_file" >"${deploy_key_file/.key/.sops.key}"
+  fi
 
-# Create deploy key in the repository.
-public_key=$(awk '{print $2}' <"$deploy_key_file.pub")
-if ! gh repo deploy-key list | grep -q "$public_key"; then
-  gh repo deploy-key add --title "$region" "$deploy_key_file.pub"
-fi
+  public_key=$(awk '{print $2}' <"$deploy_key_file.pub")
+  if ! gh repo deploy-key list | grep -q "$public_key"; then
+    gh repo deploy-key add --title "$region" "$deploy_key_file.pub"
+  fi
+}
 
-# Generate secrets.
-talos_secret_file="configs/regions/$region/talos.secret.yaml"
-if [[ ! -f "$talos_secret_file" ]]; then
-  echo "info: generating talos secrets for region: $region"
+generate_talos_secrets() {
+  talos_secret_file="$region_dir/talos.secret.yaml"
+  if [[ ! -f "$talos_secret_file" ]]; then
+    echo "info: generating talos secrets for region: $region"
 
-  talosctl gen secrets --output-file="$talos_secret_file"
-fi
-if [[ ! -f "${talos_secret_file/.secret.yaml/.sops.yaml}" ]]; then
-  sops --encrypt "$talos_secret_file" >"${talos_secret_file/.secret.yaml/.sops.yaml}"
-fi
+    talosctl gen secrets --output-file="$talos_secret_file"
+  fi
+  if [[ ! -f "${talos_secret_file/.secret.yaml/.sops.yaml}" ]]; then
+    sops --encrypt "$talos_secret_file" >"${talos_secret_file/.secret.yaml/.sops.yaml}"
+  fi
+}
+
+generate_talos_flux_patch() {
+  flux_dir="clusters/$region/flux-system"
+  flux_components="source-controller,helm-controller,kustomize-controller,notification-controller"
+
+  export REPO_URL
+  export CLUSTER
+
+  REPO_URL="ssh://$(gh repo view --json "sshUrl" --jq ".sshUrl" | tr ":" "/")"
+  CLUSTER="$region"
+
+  mkdir -p "$flux_dir"
+
+  # Configure flux components in configuration repository.
+  envsubst <"configs/flux/kustomization.tpl.yaml" >"$flux_dir/kustomization.yaml"
+  envsubst <"configs/flux/gotk-sync.tpl.yaml" >"$flux_dir/gotk-sync.yaml"
+  flux install \
+    --components="$flux_components" \
+    --export >"$flux_dir/gotk-components.yaml"
+
+  # Prepare flux manifests for Talos deployment.
+  flux_manifest="$(cat "$flux_dir/gotk-components.yaml")"
+  flux_manifest+="$(cat "$flux_dir/gotk-sync.yaml")"
+  flux_manifest+="$(flux create secret git flux-system \
+    --url="$REPO_URL" \
+    --private-key-file="$region_dir/flux.id_ed25519.key" --export)"
+
+  echo "$flux_manifest" >"talos.flux-system.yaml"
+
+  ## TODO: Set up SOPS for flux.
+}
+
+main() {
+  preflight_checks "$@"
+  configure_deploy_key
+  generate_talos_secrets
+  generate_talos_flux_patch
+}
+
+main "$@"
